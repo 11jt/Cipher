93ce42ab702b28e2a6b7ecd9653850ca2009f93d
GitHub Action <action@github.com>
2024-11-29 22:33:03 +0000

    Initialize template repository proper
    Set package name to com.github.11jt.cipher
    Set project name to Cipher
    Set modid to cipher

--- src/main/java/com/github/11jt/cipher/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/11jt/cipher/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.11jt.cipher.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
